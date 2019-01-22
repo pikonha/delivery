bee6c1fb5e86b4c4ac8e3bb81bd8ccea45d4fc07
DEV\lucas.picollo <lucas.picollo@audaces.com>
2019-01-22 17:14:46 -0200

    trying to connect with giphy's api

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const app = express()
 const axios = require('axios')
+require('dotenv').load()
 
-app.use(express.json())
+// app.use(express.json())
 
 app.get('/', async (req, res) => {
   try {
-    const reciepe = await axios.get("http://www.recipepuppy.com/api/?i=onions,garlic")
-    res.send(reciepe)
+    const reciepe = await axios.get(`http://www.recipepuppy.com/api/?i=`)
+    const giph = await axios.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=YOUR_API_KEY&limit=5")
+    res.send()
+
   } catch (err) {
     console.log(err)
   }
